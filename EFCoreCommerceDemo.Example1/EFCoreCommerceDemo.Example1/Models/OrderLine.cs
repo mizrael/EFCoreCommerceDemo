5ea47323b4c8a1921222b2c88f67f594bbcdf5cf
Davide Guida <mizrael@yahoo.it>
2019-10-14 00:27:47 +0200

    Update OrderLine.cs

--- EFCoreCommerceDemo.Example1/EFCoreCommerceDemo.Example1/Models/OrderLine.cs
+++ EFCoreCommerceDemo.Example1/EFCoreCommerceDemo.Example1/Models/OrderLine.cs
@@ -3,12 +3,18 @@
     public class OrderLine
     {
         private OrderLine() { }
+        
         public OrderLine(Product product, decimal price, int quantity)
         {
-            Price = price;
+            if(quantity < 1)
+                throw new ArgumentException("quantity has to be at least 1", nameof(quantity));
             Quantity = quantity;
-            Product = product;
+            
+            Product = product ?? throw new ArgumentNullException(nameof(product));
+            
+            Price = price;
         }
+        
         public Product Product { get; }
         public decimal Price { get; }
         public int Quantity { get; }
