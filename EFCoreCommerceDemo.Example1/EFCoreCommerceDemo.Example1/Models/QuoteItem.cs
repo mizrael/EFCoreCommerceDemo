76fce7dd945fff68e4991a9fc1cdcbbf952ab84a
Davide Guida <mizrael@yahoo.it>
2019-10-14 00:27:20 +0200

    Update QuoteItem.cs

--- EFCoreCommerceDemo.Example1/EFCoreCommerceDemo.Example1/Models/QuoteItem.cs
+++ EFCoreCommerceDemo.Example1/EFCoreCommerceDemo.Example1/Models/QuoteItem.cs
@@ -5,8 +5,10 @@
         private QuoteItem() { }
         public QuoteItem(Product product, int quantity)
         { 
+            if(quantity < 1)
+                throw new ArgumentException("quantity has to be at least 1", nameof(quantity));
             Quantity = quantity;
-            Product = product;
+            Product = product ?? throw new ArgumentNullException(nameof(product));
         }
 
         public Product Product { get; }
